60007bcc3e1456dce2a0ef0c9b2686eb576265f8
Sejin Kim <sejjj120@gmail.com>
2019-12-27 17:00:48 +0900

    feat: 리액트로 레이아웃 작성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import CreditChart from "./sections/CreditChart";
+import MajorChart from "./sections/MajorChart";
+import TotalChart from "./sections/TotalChart";
+import styled from "styled-components";
+
+const RecordInput = styled.div`
+  padding: 3rem;
+  div {
+    margin: 0 auto;
+    text-align: center;
+    font-size: 2rem;
+    background-color: whitesmoke;
+    table {
+      width: 100%;
+    }
+  }
+  button {
+    margin-top: 3rem;
+  }
+`;
+
+const pasteHandler = () => {
+  setTimeout(() => {
+    try {
+      const table = document.getElementsByTagName("table")[0];
+
+      table.style.width = "100%";
+    } catch {
+      alert("잘못된 데이터 형식입니다.");
+      document.getElementById("editableDiv").innerHTML = "";
+    }
+  });
+};
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState([]);
+
+  return (
+    <div className="App">
+      <RecordInput>
+        <div id="editableDiv" contentEditable="true" onPaste={pasteHandler} />
+        <button
+          className="button is-primary is-fullwidth is-rounded"
+          // onClick={}
+        >
+          차트 만들기
+        </button>
+      </RecordInput>
+      {data.length !== 0 && (
+        <>
+          <CreditChart data={data} />
+          <MajorChart data={data} />
+          <TotalChart data={data} />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/sections/CreditChart.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// 이수 학점 차트
+function CreditChart() {
+  return <section></section>;
+}
+
+export default CreditChart;
