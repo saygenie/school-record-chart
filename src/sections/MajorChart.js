5c60277317175a0e13b6c94f96aa425cf68bae72
Sejin Kim <sejjj120@gmail.com>
2019-12-27 21:00:28 +0900

    fix: 충돌 수정

--- src/sections/MajorChart.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// 전공 학점 차트
-function MajorChart() {
-  return <section></section>;
-}
-
-export default MajorChart;
--- src/sections/TotalChart.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// 통합 학점 차트
-function TotalChart() {
-  return <section></section>;
-}
-
-export default TotalChart;
