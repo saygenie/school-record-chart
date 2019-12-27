a48fd3bde289bb3be902c5e6556bf90f2a3fe18f
Sejin Kim <sejjj120@gmail.com>
2019-12-27 18:57:22 +0900

    feat: App 컴포넌트에 차트 컴포넌트 표시

--- src/sections/CreditChart.js
+++ src/sections/CreditChart.js
@@ -1,8 +1,44 @@
 import React from "react";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+
+function filter(data) {}
 
 // 이수 학점 차트
-function CreditChart() {
-  return <section></section>;
+function CreditChart({ data }) {
+  const filteredData = filter(data);
+
+  return (
+    <section>
+      <BarChart
+        width={500}
+        height={300}
+        data={filteredData}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="yearterm" />
+        <YAxis type="number" domain={[0, "dataMax"]} />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="averageGPA" stackId="a" fill="#8884d8" />
+        <Bar dataKey="averageGPA" stackId="a" fill="#8884d8" />
+      </BarChart>
+    </section>
+  );
 }
 
 export default CreditChart;
--- src/sections/TotalChart.js
+++ src/sections/TotalChart.js
@@ -1,8 +1,60 @@
 import React from "react";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+
+// const data = [
+//   { yearterm: "2015-1", averageGPA: "3.16" },
+//   {
+//     yearterm: "2015-2",
+//     averageGPA: "3.74"
+//   },
+//   {
+//     yearterm: "2016-1",
+//     averageGPA: "3.81"
+//   },
+//   {
+//     yearterm: "2016-2",
+//     averageGPA: "4.5"
+//   }
+// ];
+
+function filter(data) {}
 
 // 통합 학점 차트
-function TotalChart() {
-  return <section></section>;
+function TotalChart({ data }) {
+  const filteredData = filter(data);
+
+  return (
+    <section>
+      <BarChart
+        width={500}
+        height={300}
+        data={filteredData}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="yearterm" />
+        <YAxis type="number" domain={[0, "dataMax"]} />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="averageGPA" stackId="a" fill="#8884d8" />
+        <Bar dataKey="averageGPA" stackId="a" fill="#8884d8" />
+      </BarChart>
+    </section>
+  );
 }
 
 export default TotalChart;
